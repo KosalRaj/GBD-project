4138e1139f3671def2f3582583548f4ab8bd71a6
koshal <koshalshakya72@gmail.com>
2021-07-19 19:47:26 +0545

    Fix featured slider

--- script/slider-script.js
+++ script/slider-script.js
@@ -66,4 +66,38 @@ $(document).ready(function () {
 			},
 		],
 	})
+
+	$(".featured-slider").slick({
+		rtl: false, // If RTL Make it true & .slick-slide{float:right;}
+		autoplay: false,
+		// autoplaySpeed: 5000, //  Slide Delay
+		speed: 500, // Transition Speed
+		slidesToShow: 4, // Number Of Carousel
+		slidesToScroll: 4, // Slide To Move 
+		pauseOnHover: false,
+		appendArrows: $(".featured-container"), // Class For Arrows Buttons
+		prevArrow: '<button class="slider-control-prev"><span class="carousel-control-prev-icon slider-control-previous-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></button>',
+		nextArrow: '<button class="slider-control-next"><span class="carousel-control-next-icon slider-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></button>',
+		easing: "linear",
+		responsive: [
+			{
+				breakpoint: 1500, settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+			},
+			{
+				breakpoint: 1000, settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 528, settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+		],
+	})
 })
